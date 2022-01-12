02cb14765d47ae5d059d42c9927d18e7f4d8ae74
Taeyeon <Taeyeon@192.168.0.115>
2022-01-12 13:19:38 +0900

    상위 클래스

---
+++ src/test/contructorcall2/ClsA.java
@@ -0,0 +1,12 @@
+package test.contructorcall2;
+
+public class ClsA {
+
+	public ClsA() {
+		System.out.println("ClsA() 扁夯 积己磊");
+	}
+	
+	public ClsA(int n) {
+		System.out.println("ClsA(int n) 积己磊 角青 : " + n);
+	}
+}
