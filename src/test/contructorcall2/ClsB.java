002272cc39491e2e3df836dbf8a91c662cc27a6a
Taeyeon <Taeyeon@192.168.0.115>
2022-01-12 13:22:22 +0900

    하위 클래스 ClsB

---
+++ src/test/contructorcall2/ClsB.java
@@ -0,0 +1,12 @@
+package test.contructorcall2;
+
+public class ClsB extends ClsA {
+
+	public ClsB() {
+		System.out.println("ClsB() 扁夯 积己磊 角青");
+	}
+	
+	public ClsB(int n) {
+		System.out.println("ClsB(int n) 积己磊 角青 : "+ n);
+	}
+}
